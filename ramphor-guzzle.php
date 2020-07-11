13e1a591d034e88845d735d45572798b322ea92e
Puleeno Nguyen <puleeno@gmail.com>
2020-07-11 23:06:36 +0700

    autoload guzzle

---
+++ ramphor-guzzle.php
@@ -0,0 +1,19 @@
+<?php
+function load_base_guzzle()
+{
+    $base_autoload = sprintf('%s/base/autoload.php', dirname(__FILE__));
+    if (file_exists($base_autoload)) {
+        require_once $base_autoload;
+    }
+}
+
+function load_guzzle_version()
+{
+    $version_autoload = sprintf('%s/v%s/autoload.php', dirname(__FILE__), PHP_VESION_ID >= 70205 ? '7.0' : '6.5');
+    if (file_exists($version_autoload)) {
+        require_once $version_autoload;
+    }
+}
+
+load_base_guzzle();
+load_guzzle_version();
